3563fd49bc68435c72b6270813270db6a70c3d5c
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2025-03-03 12:06:35 +0000

    added code to process length wise oliogo creation

--- rate_probes.R
+++ rate_probes.R
@@ -1,6 +1,46 @@
 library(tidyverse)
-all_probes_files <- list.files(path="/g/schwab/Marco/projects/osFISH/species_seq", 
+
+## if i did seperate fro each lengthm, use this blocm to combine files:
+
+library(tidyverse)
+
+PATT <- "_probes_log.tsv"
+PATT <- "_probes_log_filtered_best4.tsv"
+all_probes_files <- list.files(path="/scratch/rheinnec/osFISH/species/", 
+                               pattern=PATT, 
+                               recursive = T,
+                               full.names = T)
+
+
+
+lapply(unique(basename(dirname(all_probes_files))), function(SPEC){
+  
+  print(SPEC)
+  
+  comb_file <- lapply(all_probes_files[which(str_detect(all_probes_files, SPEC))], read_tsv) %>%
+    bind_rows() %>%
+    mutate(identifier=paste0(identifier, length))
+  
+  write_tsv(comb_file,
+            file=file.path("/g/schwab/rheinnec/projects/osFISH/species_seq_40bp/", SPEC, paste0(SPEC, PATT)))
+  
+})
+
+### from here probe analysis
+
+
+
+
+
+
+
+all_probes_files <- list.files(path="/g/schwab/Marco/projects/osFISH/species_seq_40bp", 
                                pattern="_probes_log.tsv", 
+                               recursive = T,
+                               full.names = T)
+
+all_best4_files <- list.files(path="/g/schwab/Marco/projects/osFISH/species_seq_40bp", 
+                               pattern="best4.tsv", 
                                recursive = T,
                                full.names = T)
 
@@ -13,24 +53,45 @@ full_probes <- lapply(all_probes_files, function(file){
     )
   
 }) %>%
-  bind_rows()
+  bind_rows()%>%
+  mutate(
+    species=basename(dirname(file))
+  )
 
-
-anno_species <- full_probes %>%
+full_best4 <- lapply(all_best4_files, function(file){
+  
+  read_tsv(file, col_types = cols(.default = "c")) %>%
+    mutate(
+      file=file
+    )
+  
+}) %>%
+  bind_rows()%>%
   mutate(
     species=basename(dirname(file))
   )
 
 
+anno_species <- full_probes %>%
+  left_join(
+    full_best4 %>% select(identifier, species) %>% mutate(best4=T)
+  )
 
 
 anno_species %>%
+  filter(hitsR_abs<50) %>%
+  group_by(species) %>%
+  tally()
+
+ranked <- anno_species %>%
+#  filter(best4) %>%
   #group_by() %>%
   #filter(mismatch1_abs<1000) %>%
   group_by(species) %>%
   arrange(
-    desc(mismatch1_abs),
-    desc(class)
+    hitsR_abs,
+    mismatch1_abs
+    #desc(class)
   ) %>%
   mutate(
     rank_mismatch1_abs=seq(1,length(species))
@@ -42,9 +103,24 @@ anno_species %>%
   mutate(
     rank_class=seq(1,length(species))
   ) %>%
-  View()
+  mutate(
+    rank_sum=rank_mismatch1_abs+rank_class
+  ) %>%
+  arrange(rank_sum) %>%
+  mutate(
+    final_rank=seq(1,length(species))
+  ) 
+  #View()
 
 
+
+check <- ranked %>%
+  select(species, identifier, final_rank, best4, hitsT_abs, hitsR_abs, mismatch1_abs, mismatch2_abs, class, Tm,revCom) %>%
+  filter(final_rank<5|best4) %>%
+  arrange(species)
+
+
+write_tsv(check, file="/g/schwab/Marco/projects/osFISH/ov_table_probes.tsv")
 
 
 
